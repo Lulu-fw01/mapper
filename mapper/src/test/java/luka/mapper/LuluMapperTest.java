dd760f933851af6154bc17e346b0441dd2f0c81c
luka <limarkov@edu.hse.ru>
2022-02-19 00:51:38 +0300

    Created class

---
+++ mapper/src/test/java/luka/mapper/LuluMapperTest.java
@@ -0,0 +1,32 @@
+package luka.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LuluMapperTest {
+
+    @Test
+    void readFromString() {
+    }
+
+    @Test
+    void read() {
+    }
+
+    @Test
+    void testRead() {
+    }
+
+    @Test
+    void writeToString() {
+    }
+
+    @Test
+    void write() {
+    }
+
+    @Test
+    void testWrite() {
+    }
+}
